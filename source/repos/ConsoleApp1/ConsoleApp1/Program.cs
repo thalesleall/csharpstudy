ece24ef0c76aaeab148c95b90e5940a5867b248e
Thales Leal <125505752+thalesleall@users.noreply.github.com>
2023-08-07 19:35:05 -0300

    Aulas

---
+++ source/repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1_Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int numero = 0;
+             int N = 10;
+             string nome = "";
+             int idade = 0;
+
+             Console.WriteLine("Hello World!!");
+             Console.WriteLine(1 + 1);
+             Console.WriteLine("Soma: "  + (1 + 1));  //WRITE: MESMA LINHA; WRITELINE: PULA DE LINHA
+             Console.Clear();
+             Console.Write("Digite seu nome : ");
+             nome = Console.ReadLine();
+             Console.Write("Digite sua idade: ");
+             idade = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("O nome é igual a: " + nome + ", e a idade é igual a: " + idade);
+
+             Console.ReadKey(); */
+
+            /*int n1 = 0;
+            int n2 = 0;
+
+            Console.Write("Digite o primeiro numero: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Digite o segundo numero: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O primeiro numero digitado foi: " + n1 );
+            Console.WriteLine("O segundo numero digitado foi: " + n2);
+
+            Console.WriteLine("A soma dos dois numeros é igual a: " + (n1 + n2));*/
+
+            Console.Write("Digite seu nome: ");
+            string Nome = Console.ReadLine();
+            Console.Write("Digite a cidade que mora: ");
+            string Cidade = Console.ReadLine();
+            Console.Write("Digite o estado em que você mora: ");
+            string Estado = Console.ReadLine();
+            Console.Write("Digite seu telefone: ");
+            string Telefone = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Cidade: " + Cidade);
+            Console.WriteLine("Estado: " + Estado);
+            Console.WriteLine("Telefone: " + Telefone);
+        }
+    }
+}
---
+++ source/repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//CONDICIONAL
+            // Simples
+            // Composta
+            // Encadeada
+            
+            int a = 0; 
+            Console.WriteLine("Digite um numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 10)
+            {
+                a = a + 1;
+            }
+            else if (a < 2) 
+            {
+                a = a + 2;
+            }
+
+            Console.WriteLine("Valor de A: " + a);
+            Console.ReadKey();*/
+
+            //int a = 0;
+            //int b = 0;
+            //int c = 0;
+            //int d = 0;
+
+            //Console.Write("Digite um numero para A: ");
+            //a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Digite um numero para B: ");
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Digite um numero para C: ");
+            //c = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Digite um numero para D: ");
+            //d = Convert.ToInt32(Console.ReadLine());
+
+            //if (a == b && b == c && c == d)
+            //{
+            //    Console.WriteLine(" _____");
+            //    Console.WriteLine("|     |");
+            //    Console.WriteLine("|     |");
+            //    Console.WriteLine("|     |");
+            //    Console.WriteLine(" -----");
+
+            //    Console.Write("Parabéns isso é um quadrado!");
+            //}
+            //else
+            //{
+            //    Console.Write("Isso não é um quadrado :(");
+            //}
+            //Console.ReadKey();
+
+            //int a = 0;
+            //int b = 0;
+
+            //Console.Write("Digite um numero para A: ");
+            //a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Digite um numero para B: ");
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //if(a != b)
+            //{
+            //    if(a > b)
+            //    {
+            //        Console.Write("O numero maior é o A, que é igual a: " + a);
+            //    }
+            //    else if(a < b)
+            //    {
+            //        Console.Write("O numero maior é o A, que é igual a: " + b);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.Write("Os numeros são iguais!");
+            //}
+            //Console.ReadKey();
+
+            double a = 0.0;
+            double b = 0.0;
+
+            string equacao = "";
+
+
+            Console.Write("Digite um numero para A: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite um numero para B: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("A - para somar os numeros\nS - para subtrair os numeros\nM - para dividir os numeros\nD - para dividir os numeros\nDigite: ");
+            equacao = Console.ReadLine();
+
+            if(equacao == "A")
+            {
+                Console.WriteLine("O resultado da soma de A por B é: " + (a + b));
+            }
+            else if(equacao == "S")
+            {
+                Console.WriteLine("O resultado da subtração de A por B é: " + (a - b));
+            }
+            else if(equacao == "M")
+            {
+                Console.WriteLine("O resultado da multiplicação de A por B é: " + (a * b));
+            }
+            else if(equacao == "D") 
+            {
+                Console.WriteLine("O resultado da divisao de A por B é: " + (a / b));
+            }
+            else
+            {
+                Console.WriteLine("Digite uma equação valida");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
